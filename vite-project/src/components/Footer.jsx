7f1c4b57c3168159d84ff33931cafc17a0e13c24
raghuvamshidharreddy <raghuvamshidharreddy@gmail.com>
2026-03-10 15:02:15 +0530

    new

---
+++ vite-project/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+function Footer(){
+    return (
+    <div class="App-Footer">
+    <h3>Copyright 2026 @ licensed by Praveen Nair</h3>
+    </div>
+)}
+export {Footer}
---
+++ vite-project/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import "./Header.css"
+function Header(){
+    return <div className="App-Header">
+        <h1>My appliction</h1>
+        <ul>
+            <li>Home</li>
+            <li>Products</li>
+            <li>User</li>
+            <li>Login</li>
+            <li>Logout</li>
+        </ul>
+    </div>
+}
+export {Header}
